70af811d59655e548943cda3a5dcd38ba92e360e
Sudhamsu <gurijalasudhamsu2000@gmail.com>
2024-06-17 12:28:49 +0530

    Adds github actions for deployment

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// @ts-check
+ 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/my-page",
+  reactStrictMode: true
+}
+ 
+module.exports = nextConfig
